d0edc5b6df6a75d5af53652b1fdc5de618e6343a
Pyetro-Silva <rayanneleal28@gmail.com>
2024-03-15 11:55:40 -0300

    função: oppening, jumplines, get_op

--- Calculator/main.lua
+++ Calculator/main.lua
@@ -1,1 +1,26 @@
 --Calculadora em lua uwu
+
+--Iniciando o programa
+function Oppening()
+    print('\t=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+    print('\t                   Calculator')
+    print('\t=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+end
+
+--Função para pular linhas
+function Jump_Lines(num)
+    for i=1, num do
+        print('\n')
+    end
+end 
+
+
+--Pegando os input sobre as operações
+function Get_op()
+    Jump_Lines(1)
+    print("a")
+end
+
+
+Oppening()
+Get_op()
